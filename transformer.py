37f50d37ca4223f2bc23bbea1213465b093ca5d8
jwyyy <jwyao@ucdavis.edu>
2021-01-10 21:27:45 -0800

    Made some changes.

---
+++ transformer.py
@@ -0,0 +1,9 @@
+# reference post: https://towardsdatascience.com/how-to-code-the-transformer-in-pytorch-24db27c8f9ec
+# reference : https://github.com/SamLynnEvans/Transformer
+
+# Note:
+# inputs for MultiHead Attention in encoders and decoders are different.
+# 
+
+import torch
+import numpy as np
